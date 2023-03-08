e8b78692dbe1310b0acd8e0993e4fbc87ea40d8e
Kamil <qkiz288@gmail.com>
2023-03-08 15:30:17 +0100

    Added additional mock data

--- src/repositories/units/units-mock.repository.ts
+++ src/repositories/units/units-mock.repository.ts
@@ -8,19 +8,31 @@ export class UnitsMockRepository implements UnitsRepository {
             name: "SL unit 1",
             people: [{
                 name: 'John Doe',
-                meetings: [{
-                    comment: 'Everything good',
-                    attrition: 'None',
-                    date: '01-02-2023'
-                }]
+                meetings: [
+                    {
+                        comment: 'Everything good',
+                        attrition: 'None',
+                        date: '01-02-2023'
+                    },
+                    {
+                        comment: 'Doing great',
+                        attrition: 'None',
+                        date: '11-20-2022'
+                    }]
             },
             {
                 name: 'Tom Thompson',
-                meetings: [{
-                    comment: 'Coffe could be more tasty',
-                    attrition: 'Low',
-                    date: '02-25-2023'
-                }]
+                meetings: [
+                    {
+                        comment: 'Coffe could be more tasty',
+                        attrition: 'Low',
+                        date: '02-25-2023'
+                    },
+                    {
+                        comment: 'Bananas could be more fresh',
+                        attrition: 'Low',
+                        date: '01-25-2023'
+                    }]
             }]
         },
         {
@@ -28,19 +40,31 @@ export class UnitsMockRepository implements UnitsRepository {
             name: "SL unit 2",
             people: [{
                 name: 'Kevin Townsend',
-                meetings: [{
-                    comment: 'Other sevelopers are not writing unit tests',
-                    attrition: 'Medium',
-                    date: '02-23-2023'
-                }]
+                meetings: [
+                    {
+                        comment: 'Other sevelopers are not writing unit tests',
+                        attrition: 'Medium',
+                        date: '02-23-2023'
+                    },
+                    {
+                        comment: 'There is no code review process',
+                        attrition: 'Medium',
+                        date: '01-23-2023'
+                    }]
             },
             {
                 name: 'Terry Crews',
-                meetings: [{
-                    comment: 'Office gym is locked',
-                    attrition: 'High',
-                    date: '01-30-2023'
-                }]
+                meetings: [
+                    {
+                        comment: 'Office gym is locked',
+                        attrition: 'High',
+                        date: '01-30-2023'
+                    },
+                    {
+                        comment: 'Weights are too light',
+                        attrition: 'High',
+                        date: '12-14-2022'
+                    }]
             }]
         }
     ];
@@ -57,5 +81,5 @@ export class UnitsMockRepository implements UnitsRepository {
 
         return units;
     }
-    
+
 }
